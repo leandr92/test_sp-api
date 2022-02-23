8c92e3404741b19f5a1e51255f3ab44fab569a77
Eldar Mingaliev <epirsa6@gmail.com>
2022-02-23 14:26:39 +0300

    adding some comments end console output

--- main.py
+++ main.py
@@ -11,23 +11,30 @@ credentials = dict(
 )
 
 
-# test get orders without restricted data
+# test get orders without restricted data work completely
 orders_without_rd = Orders(credentials=credentials).get_orders(
     LastUpdatedAfter=(datetime.utcnow() - timedelta(days=1)).isoformat()
 )
 
+print("orders_without_rd")
+print(orders_without_rd)
 
-# Test get RDT token only
+
+# Test get RDT token only don't work
 rdt_token = Tokens(credentials=credentials).create_restricted_data_token(restrictedResources=[{
      "method": "GET",
      "path": "/orders/v0/orders",
      "dataElements": ['shippingAddress']
     }
 ])
+print("rdt_token")
+print(rdt_token)
 
-
-# test get orders with restricted data
+# test get orders with restricted data don't work
 orders_with_rd = Orders(credentials=credentials).get_orders(
     RestrictedResources=['shippingAddress'],
     LastUpdatedAfter=(datetime.utcnow() - timedelta(days=1)).isoformat()
 )
+
+print("orders_with_rd")
+print(orders_with_rd)
